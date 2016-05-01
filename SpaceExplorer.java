bc2f39ed4f2ad38aaa7d0607632cbabc7de7ce4d
tyrtheexile <zygar84@gmail.com>
2016-05-01 11:48:02 -0700

    Day 1 Still at home :p

--- SpaceExplorer.java
+++ SpaceExplorer.java
@@ -4,6 +4,6 @@ public class SpaceExplorer
 {
 	public static void main(String[] args) {
 		System.out.println("Okay I am getting the hang of this");
-		System.out.println("Great Job");
+		System.out.println("Still Testing though");
 	}
 }
