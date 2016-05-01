b6a277c967e07515db6c81239ce48f5b3d90e471
zygar84 <zygar84@gmail.com>
2016-05-01 11:47:18 -0700

    Day1 At home

--- SpaceExplorer.java
+++ SpaceExplorer.java
@@ -3,7 +3,7 @@
 public class SpaceExplorer
 {
 	public static void main(String[] args) {
-		System.out.println("Testing");
+		System.out.println("Okay I am getting the hang of this");
 		System.out.println("Great Job");
 	}
 }
